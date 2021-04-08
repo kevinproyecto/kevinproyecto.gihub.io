4fc0d44dc827cf01d2a925647f9e83c92eb3999d
kevinproyecto <82137520+kevinproyecto@users.noreply.github.com>
2021-04-08 01:29:10 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyB2SD3RwDHuBtR7ytHun2dKtxD5j1m65XQ",
+    authDomain: "kevinproyecto43.firebaseapp.com",
+    projectId: "kevinproyecto43",
+    storageBucket: "kevinproyecto43.appspot.com",
+    messagingSenderId: "230196695011",
+    appId: "1:230196695011:web:678f03d1d121cdf4b85ad6",
+    measurementId: "G-MGDRDPR9JC"
 });
